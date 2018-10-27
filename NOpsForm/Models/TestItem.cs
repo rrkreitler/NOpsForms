00eecdf2c92eef97937ff639932360c6b5af93ef
davek@northbendeng.com <davek@northbendeng.com>
2018-10-27 07:12:09 -0700

    Fixed label mouseEnter

---
+++ NOpsForm/Models/TestItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOpsForm.Models
+{
+    public class TestItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
